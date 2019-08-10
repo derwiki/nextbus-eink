e7e37e29a18e7930f64f62540791ef29717a27b9
Adam Derewecki <derewecki@gmail.com>
2019-08-09 18:03:29 -0700

    try/catch loop for nextbus API errors

--- nextbus.py
+++ nextbus.py
@@ -8,7 +8,8 @@ import predictions
 SCREEN = Papirus()
 SCREEN.update()
 
-def display(s, full = False):
+
+def display(s, full=False):
     img, _ = bitmaps.display_text_prop(s, display_size=(200, 96), char_size=2)
     img.save("tmp.bmp")
     file = Image.open("tmp.bmp")
@@ -18,12 +19,21 @@ def display(s, full = False):
     else:
         SCREEN.partial_update()
 
+
 i = 0
 while True:
-   i += 1
-   now = datetime.datetime.now().strftime('%-I:%M:%S %p')
-   preds = predictions.request_predictions()
-   route_lines = '\n'.join(['  %s: %s' % (line, ', '.join(times)) for line, times in preds.items()])
-   buffer = '%s\n%s\n%s' % ('        %s' % now, '  NextBus', route_lines)
-   display(buffer, full=i % 3 == 0)
-   time.sleep(10)
+    try:
+        preds = predictions.request_predictions()
+        i += 1
+        route_lines = '\n'.join([
+            '  %s: %s' % (line, ', '.join(times))
+            for line, times
+            in preds.items()
+        ])
+        now = datetime.datetime.now().strftime('%-I:%M:%S %p')
+        buffer = '%s\n%s\n%s' % ('        %s' % now, '  NextBus', route_lines)
+        display(buffer, full=i % 3 == 0)
+    except Exception as e:
+        print(str(e))
+
+    time.sleep(10)
